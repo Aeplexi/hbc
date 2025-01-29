0e5622dfaa12ce93f52e5e8ed8105c3a06e1a318
Korbo <korbin.m.marshall@gmail.com>
2025-01-28 22:16:11 -0600

    add wii mini, wii family edition, ndev detection

--- channel/channelapp/source/wiiinfo.c
+++ channel/channelapp/source/wiiinfo.c
@@ -4,7 +4,7 @@
 
 static const char* devDolphin [[gnu::aligned(0x20)]] = "/dev/dolphin";
 static int dolphin_fd = ~0;
-char* unknown_wii_model_message = "Unknown model";
+extern int __CONF_GetTxt(const char *name, char *buf, int length);
 
 // thanks Naim2000/thepikachugamer for this method of detecting dolphin
 bool is_dolphin() {
@@ -16,18 +16,58 @@ bool is_dolphin() {
 	return dolphin_fd > 0;
 }
 
-// maybe use an enum later?  needs revamp, wii family edition detection
-char* get_wii_model()
-{
-    if (is_dolphin())
-    {
+int check_setting() {
+	char code[4];
+	char model[13];
+	char ret;
+
+	ret = __CONF_GetTxt("CODE", code, 4);
+	if (ret < 0)
+		return ret;
+
+	ret = __CONF_GetTxt("MODEL", model, 13);
+	if (ret < 0)
+		return ret;
+
+	if (model[2] == 'T') {
+		ret = 3;
+	}
+
+	if ((code[0] == 'K') | (model[4] == '1')) {
+		ret = 1;
+	} else if ((code[0] == 'H') | (model[4] == '2')) {
+		ret = 2;
+	}
+	return ret;
+}
+
+// TODO: maybe use an enum later?  needs revamp
+char* get_wii_model() {
+	int setting = check_setting();
+
+    if (is_dolphin()) {
         return "Dolphin";
-    }
+	}
 
-    if (is_vwii())
-    {
+	if (setting == 1) {
+		return "Wii Family Edition";
+	}
+
+	if (setting == 2) {
+		return "Wii Mini";
+	}
+
+	if (setting == 3) {
+		return "NDEV";
+	}
+
+    if (is_vwii()) {
         return "Wii U";
-    }
+	}
 
-    return "Wii";
+	if (setting < 0) {
+		return "Unknown model";
+	}
+
+	return "Wii";
 }
