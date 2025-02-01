7a29c017e43666697acbb1a8c0f069a07418ed34
Korbo <korbin.m.marshall@gmail.com>
2025-02-01 12:30:26 -0600

    Update wiiinfo.h

--- channel/channelapp/source/wiiinfo.h
+++ channel/channelapp/source/wiiinfo.h
@@ -9,7 +9,7 @@ char* get_wii_model();
 s32 check_connection();
 char* check_connection_str();
 char* get_serial();
-char* check_setting();
+int check_setting();
 char* get_area();
 char* is_priiloader_installed();
 char* is_bootmii_installed();
