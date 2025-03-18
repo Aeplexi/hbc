74b830bd788548cc40dfd7b2c529d4e434eeecd9
Aeplexi <markisthebest2153@gmail.com>
2025-03-17 20:45:24 -0400

    remove button_y_offset

--- channel/channelapp/source/m_main.c
+++ channel/channelapp/source/m_main.c
@@ -142,10 +142,6 @@ void m_main_deinit(void) {
 	v_m_main = NULL;
 }
 
-int button_y_offset(int y, int multiplier) {
-	return y + theme_gfx[THEME_BUTTON]->h * multiplier / 2;
-}
-
 void m_main_theme_reinit(void) {
 	u16 x, y = 0, yadd = 0, button_count = 0;
 	int i;
@@ -234,7 +230,6 @@ void m_main_theme_reinit(void) {
 			parent_menu = MENU_HOME;
 
 			yadd = theme_gfx[THEME_BUTTON]->h*2/3;
-			y = button_y_offset(80, 2) - (theme_gfx[THEME_BUTTON]->h / 2) + 24;
 
 			widget_button (&v_m_main->widgets[0], x, y, 0, BTN_NORMAL,
 						   _("Reset Theme"));
